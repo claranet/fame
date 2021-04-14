984b5139432c43aa8be837aae2aac7979fa76170
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2021-04-14 14:18:49 +0200

    AZ-393: Generic Log Analytics Queries to SignalFx implementation - improvements

--- log_analytics_queries/app.py
+++ log_analytics_queries/app.py
@@ -5,6 +5,7 @@ import logging
 import os
 
 import signalfx
+import yaml
 from azure import functions as func
 from dateutil.parser import parse
 
@@ -36,11 +37,18 @@ def run():
     log_analytics_workspace_id = os.environ.get('LOG_ANALYTICS_WORKSPACE_GUID')
     creds = credentials.get_credentials()
 
-    queries = {}
-    for path in glob.glob(os.path.join(os.path.dirname(__file__), 'queries', '*.query')):
-        metric = os.path.splitext(os.path.basename(path))[0]
+    extra_dimensions = {couple.split("=")[0]: couple.split("=")[1]
+                        for couple in os.environ.get('SFX_EXTRA_DIMENSIONS', '').split(",")
+                        }
+
+    queries_config = []
+    for path in glob.glob(os.path.join(os.path.dirname(__file__), 'queries', '*.yaml')):
         with open(path) as f:
-            queries[metric] = f.read()
+            data = yaml.safe_load(f)
+            if not data.get('metric_name', False) or not data.get('metric_type', False) or not data.get('query', False):
+                raise ValueError(f'Configuration file f{os.path.basename(path)} does not contain properties '
+                                 f'"metric_name", "metric_type" and "query')
+            queries_config.append(data)
 
     sfx_clt = signalfx.SignalFx(api_endpoint=f'https://api.{sfx_realm}.signalfx.com',
                                 ingest_endpoint=f'https://ingest.{sfx_realm}.signalfx.com',
@@ -49,31 +57,32 @@ def run():
     with sfx_clt.ingest(sfx_token) as sfx:
         atexit.register(sfx.stop)
 
-        for metric, query in queries.items():
-            metric_name = '.'.join(metric.split('.')[1:])
-            metric_kind = metric.split('.')[0]
-
+        for query_data in queries_config:
             sfx_values = []
+            logger.info(f"Querying and sending metric f{query_data['metric_name']}")
 
-            # TODO add logging
-            data = log_analytics.run_query(query, log_analytics_workspace_id, creds)
+            logger.debug(f"Executing query f{query_data['query']}")
+            data = log_analytics.run_query(query_data['query'], log_analytics_workspace_id, creds)
             cols = [col['name'] for col in data['tables'][0]['columns']]
-            # TODO have strict checking of those columns
-            ix_timestamp = cols.index('timestamp')
-            ix_metric_value = cols.index('metric_value')
+            try:
+                ix_timestamp = cols.index('timestamp')
+                ix_metric_value = cols.index('metric_value')
+            except ValueError:
+                raise ValueError('Columns "timestamp" and "metric_value" must exist in the query results')
+
             cols.pop(ix_timestamp)
             cols.pop(ix_metric_value - 1)
             for row in data['tables'][0]['rows']:
                 timestamp = row.pop(ix_timestamp)
                 metric_value = row.pop(ix_metric_value - 1)
                 sfx_values.append({
-                        'metric': metric_name,
+                        'metric': query_data.get('metric_name'),
                         'value': metric_value,
                         'timestamp': parse(timestamp).timestamp() * 1000,
                         # TODO add function tags as dimensions
-                        'dimensions': dict(zip(cols, row))
+                        'dimensions': {**dict(zip(cols, row)), **extra_dimensions}
                     })
-            sfx.send(**{f"{metric_kind}s": sfx_values})
+            sfx.send(**{f"{query_data.get('metric_type')}s": sfx_values})
 
 
 if __name__ == "__main__":
